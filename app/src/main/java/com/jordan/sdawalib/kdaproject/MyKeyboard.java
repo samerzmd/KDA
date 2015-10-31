c8da2b85c19f00e0468c742b2a0fab287ee0012b
samerzmd <samerzmd@gmail.com>
2015-10-31 14:24:35 +0200

    sometimes i cry... all day

--- app/src/main/java/com/jordan/sdawalib/kdaproject/MyKeyboard.java
+++ app/src/main/java/com/jordan/sdawalib/kdaproject/MyKeyboard.java
@@ -96,6 +96,15 @@ public class MyKeyboard extends InputMethodService implements KeyboardView.OnKey
                 ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
 
                 break;
+            case -11:
+                keyboard = new Keyboard(this, R.xml.symbols);
+                kv.setKeyboard(keyboard);
+                break;
+            case -12:
+
+                keyboard = new Keyboard(this, R.xml.qwerty);
+                kv.setKeyboard(keyboard);
+                break;
             default:
                 char code = (char) primaryCode;
                 if(Character.isLetter(code) && caps) {
